879daf372e068b1e6404557ba30bc2a64e20fa6b
Your Name <you@example.com>
2019-06-12 18:55:08 +0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { enableProdMode } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TimerComponent } from './timer/timer.component';
+import { ButtonsComponent } from './timer/timer.button.component';
+import { TimerService } from './timer/timer.service';
+ 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [TimerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+{
+  enableProdMode();
+}
